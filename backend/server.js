d96b6eff1f37e946f38ea208cedb5b44b3f4ec2d
vinayaksiddhi <siddhiv171@gmail.com>
2026-03-23 14:15:26 +0530

    added Backend

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+console.log("SERVER FILE STARTED 🔥");
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000 🚀");
+});
