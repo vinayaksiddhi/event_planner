4253da3299f864060d6d808e20ef5d9faf04d059
Siddhi Vinayak <siddhiv171@gmail.com>
2026-03-28 10:34:08 +0530

    Delete frontend/src/services/supabase.js

--- frontend/src/services/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  "https://mzvmtcqznenqlvchhouk.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16dm10Y3F6bmVucWx2Y2hob3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNzI0NTEsImV4cCI6MjA4OTg0ODQ1MX0.Ut7pah6-mF4z0S_UIDz5tlSqLrTmW9P0iMNRGJeKyCM"
-);
-
-export default supabase;
