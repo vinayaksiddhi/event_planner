e70588bd70f95c2218b74f2035b58a146dd45bbd
vinayaksiddhi <siddhiv171@gmail.com>
2026-04-03 02:23:49 +0530

    fix: correct css import case sensitivity for deployment 2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import AuthProvider from "./context/AuthContext";
 import "./App.css";
 // OPTIONAL (recommended if you have CSS)
 import "./index.css";
-import "./App.css"
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
